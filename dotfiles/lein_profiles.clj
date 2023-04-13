5ce578f2d2d8d9bbe78a04ac24861dd6b88ff6b3
Mario Giampietri <mario.giampietri@gmail.com>
2023-04-13 20:34:26 +0100

    more settings in lein profiles

--- dotfiles/lein_profiles.clj
+++ dotfiles/lein_profiles.clj
@@ -1,3 +1,5 @@
 {:repl {:dependencies [[nrepl "1.0.0"]]
+        ;; :repl-options {:init (-main)}
 	:plugins [[cider/cider-nrepl "0.28.5"]
-                  [mx.cider/enrich-classpath "1.9.0"]]}}
+                  [mx.cider/enrich-classpath "1.9.0"]]}
+ :user {:plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]]}}
