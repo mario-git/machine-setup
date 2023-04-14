bc02a1014d4f077f6b32e859553f30594317aff6
Mario Giampietri <mario.giampietri@gmail.com>
2023-04-14 20:46:59 +0100

    added tools.trace to lein profiles

--- dotfiles/lein_profiles.clj
+++ dotfiles/lein_profiles.clj
@@ -2,4 +2,5 @@
         ;; :repl-options {:init (-main)}
 	:plugins [[cider/cider-nrepl "0.28.5"]
                   [mx.cider/enrich-classpath "1.9.0"]]}
- :user {:plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]]}}
+ :user {:dependencies [[org.clojure/tools.trace "0.7.11"]]
+        :plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]]}}
