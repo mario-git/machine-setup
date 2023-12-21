fd2070ca5183645aaeae36eddb4afbfb83ac32db
Mario Giampietri <mario.giampietri@gmail.com>
2023-12-21 16:26:23 +0000

    enter criterium

--- dotfiles/lein_profiles.clj
+++ dotfiles/lein_profiles.clj
@@ -2,5 +2,6 @@
         ;; :repl-options {:init (-main)}
 	:plugins [[cider/cider-nrepl "0.28.5"]
                   [mx.cider/enrich-classpath "1.9.0"]]}
- :user {:dependencies [[org.clojure/tools.trace "0.7.11"]]
+ :user {:dependencies [[criterium "0.4.6"]
+                       [org.clojure/tools.trace "0.7.11"]]
         :plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]]}}
