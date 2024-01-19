caafa2556fe6833f6a6dee52ee04e72778ed0ca3
Mario Giampietri <mario.giampietri@gmail.com>
2024-01-19 10:44:06 +0000

    enter yagni

--- dotfiles/lein_profiles.clj
+++ dotfiles/lein_profiles.clj
@@ -4,4 +4,5 @@
                   [mx.cider/enrich-classpath "1.9.0"]]}
  :user {:dependencies [[criterium "0.4.6"]
                        [org.clojure/tools.trace "0.7.11"]]
-        :plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]]}}
+        :plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]
+                  [venantius/yagni "0.1.7"]]}}
