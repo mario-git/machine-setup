76629dfa7477e262a3fbca938c23215c58604d42
Mario Giampietri <mario.giampietri@gmail.com>
2024-07-16 17:22:25 +0100

    updated lein profile deps

--- dotfiles/lein_profiles.clj
+++ dotfiles/lein_profiles.clj
@@ -1,7 +1,7 @@
-{:repl {:dependencies [[nrepl "1.0.0"]]
-	:plugins [[cider/cider-nrepl "0.28.5"]
-                  [mx.cider/enrich-classpath "1.9.0"]]}
+{:repl {:dependencies [[nrepl "1.2.0"]]
+	:plugins [[cider/cider-nrepl "0.49.1"]
+                  [mx.cider/enrich-classpath "1.19.3"]]}
  :user {:dependencies [[criterium "0.4.6"]
-                       [org.clojure/tools.trace "0.7.11"]]
+                       [org.clojure/tools.trace "0.8.0"]]
         :plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]
                   [venantius/yagni "0.1.7"]]}}
