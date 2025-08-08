e61b236dbaa7fd4d4df5402b41709087ce7f064c
Mario Giampietri <mario.giampietri@gmail.com>
2025-08-08 22:38:35 +0100

    #p

--- dotfiles/lein_profiles.clj
+++ dotfiles/lein_profiles.clj
@@ -2,6 +2,8 @@
 	:plugins [[cider/cider-nrepl "0.49.1"]
                   [mx.cider/enrich-classpath "1.19.3"]]}
  :user {:dependencies [[criterium "0.4.6"]
+                       [dev.weavejester/hashp "0.4.0"]
                        [org.clojure/tools.trace "0.8.0"]]
+        :injections [(require 'hashp.preload)]
         :plugins [[lein-ancient "1.0.0-RC3" :exclusions [org.clojure/clojure]]
                   [venantius/yagni "0.1.7"]]}}
